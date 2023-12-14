d826733d0a521e7660727310077588d4296ebfdb
Chandan Somani <chandan.somani@newvision-software.com>
2023-12-14 13:48:36 +0530

    Test Commit & Push with csNV github profile on a project of cs gmail acc's repo

---
+++ IOTAppDashboardAPI/Lib.cs
@@ -0,0 +1,11 @@
+﻿namespace IOTAppDashboardAPI
+{
+    public class Lib
+    {
+        // This is a demo class lib
+        // Use this lib to implement utility function that can be used with help instantating an object of this class
+        // Also this is a demo class created to test Git Permission setting with VS 2022
+        // This is a project in cs gmail account and push & commits are done from nvcs account.
+        // Test Result -> 
+    }
+}
